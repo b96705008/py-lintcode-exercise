0818bc71031d15aa7116b419785aef04a444ca10
b96705008 <b96705008@gmail.com>
2018-05-26 13:18:53 +0800

    refactor lca 2

--- ch3/lowest_common_ancestor_3.py
+++ ch3/lowest_common_ancestor_3.py
@@ -30,46 +30,92 @@ root1.right = TreeNode(7)
 root1.right.left = TreeNode(5)
 root1.right.right = TreeNode(6)
 
+class Status(object):
 
-def find_lowest_common_ancestor(root, v1, v2):
-	lca = None
-	paths = {}
+	def __init__(self, A, B):
+		self.A = A
+		self.B = B
+		self.exist_A = False
+		self.exist_B = False
+		self.ancestor = None
 
-	if v1 is None or v2 is None:
-		return lca
+	def set_ancestor(self, node):
+		if self.ancestor is None and node is not None:
+			self.ancestor = node
 
-	def traverse(root, values, prefix, paths):
+	def find(self, node):
+		if node == self.A:
+			self.exist_A = True
+
+		if node == self.B:
+			self.exist_B = True
+
+		return self
+
+	def exist_num(self):
+		cnt = 0
+		if self.exist_A:
+			cnt = cnt + 1
+
+		if self.exist_B:
+			cnt = cnt + 1
+
+		return cnt
+
+	def merge(self, other):
+		if not self.exist_A and other.exist_A:
+			self.exist_A = True
+		
+		if not self.exist_B and other.exist_B:
+			self.exist_B = True
+
+		return self
+
+
+def find_lowest_common_ancestor(root, A, B):
+
+	if A is None or B is None:
+		return None
+
+	def divide_conquer(root, A, B):
+		my_status = Status(A, B)
+
 		if root is None:
-			return
+			return my_status
 
-		prefix.append(root.val)
-		if root.val in values:
-			paths[root.val] = prefix[:] # snapshot
+		my_status.find(root)
+		left = divide_conquer(root.left, A, B)
+		right = divide_conquer(root.right, A, B)
 
-		traverse(root.left, values, prefix, paths)
-		traverse(root.right, values, prefix, paths)
-		prefix.pop() # back track
+		if left.exist_num() == 2:
+			return left
 
-	traverse(root, [v1, v2], [], paths)
+		if right.exist_num() == 2:
+			return right
 
-	if v1 not in paths or v2 not in paths:
-		return lca
+		my_status.merge(left).merge(right)
+		if my_status.exist_num() == 2:
+			my_status.set_ancestor(root)
 
-	i = 0
-	j = 0
-	while i < len(paths[v1]) and j < len(paths[v2]):
-		if paths[v1][i] == paths[v2][j]:
-			lca = paths[v1][i]
+		return my_status
+
+	return divide_conquer(root, A, B).ancestor
+
+
+def testing(root, A, B, ans):
+	result = find_lowest_common_ancestor(root1, A, B)
+	if result is None:
+		if ans is None:
+			print True
 		else:
-			break
-		i += 1
-		j += 1
+			print False
+	else:
+		print result.val == ans
 
-	return lca
-
-
-print find_lowest_common_ancestor(root1, 3, 5) == 4
-print find_lowest_common_ancestor(root1, 5, 6) == 7
-print find_lowest_common_ancestor(root1, 6, 7) == 7
-print find_lowest_common_ancestor(root1, 3, 6) == 4
-
+testing(root1, root1.left, root1.right.left, 4)
+testing(root1, root1.right.left, root1.right.right, 7)
+testing(root1, root1.right.right, root1.right, 7)
+testing(root1, root1.left, root1.right.right, 4)
+testing(root1, root1.left, TreeNode(9), None)
+testing(root1, TreeNode(11), TreeNode(9), None)
+testing(root1, None, root1.left, None)
